951e7e3eb939e0d34b5c113087a4178f902cafc3
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-03-26 12:12:24 +0530

    Feature: Movie controller added [Movie Controller TODO](./src/controllers/movies.js) - TODO add fetch cast - TODO add fetch awards - TODO add fetch reviews

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -1,10 +1,134 @@
+import {
+  createMovie,
+  deleteMovieById,
+  getMovieById,
+  getMovies,
+  updateMovieById,
+} from "../services/movieService.js";
+
 /**
  * User routes
  * @param {import("fastify").FastifyInstance} fastify - Fastify instance
  * @param {Object} _opts - Fastify options (unused)
  */
 export default async function movieController(fastify, _opts) {
-  fastify.get("/movies", function (_request, reply) {
+  fastify.get("/hello/movies", function (_request, reply) {
     reply.send({ controller: "movies" });
+  });
+
+  fastify.get("/movies", async (request, reply) => {
+    const {
+      page = 1,
+      pageSize = 10,
+      sortBy,
+      sortDirection,
+      ...filters
+    } = request.query;
+
+    try {
+      const movies = await getMovies(
+        fastify.db,
+        Number(page),
+        Number(pageSize),
+        {
+          sortBy,
+          sortDirection,
+          filters,
+        },
+      );
+
+      reply.send(movies);
+    } catch (error) {
+      console.error(error);
+      reply.code(500).send({ error: error.message });
+    }
+  });
+
+  fastify.get("/movies/:movie_id", async (request, reply) => {
+    const { movie_id } = request.params;
+
+    try {
+      const movie = await getMovieById(fastify.db, movie_id);
+
+      if (!movie) {
+        return reply.code(404).send({ error: "Movie not found" });
+      }
+
+      // TODO add fetch cast aise movies.cast = []
+      // TODO add fetch awards aise movies.awards = []
+      // TODO add fetch reviews aise movies.reviews = []
+
+      reply.send(movie);
+    } catch (error) {
+      reply.code(500).send({ error: error.message });
+    }
+  });
+
+  fastify.post("/movies", async (request, reply) => {
+    const { title, genre, director, releaseYear, language } = request.body;
+
+    try {
+      const movie = await createMovie(fastify.db, {
+        title,
+        genre,
+        director,
+        releaseYear,
+        language,
+      });
+      reply.code(201).send({ message: "Movie created successfully", movie });
+    } catch (error) {
+      reply.code(500).send({ error: error.message });
+    }
+  });
+
+  fastify.put("/movies/:movie_id", async (request, reply) => {
+    const { movie_id } = request.params;
+    const { title, genre, director, releaseYear, language } = request.body;
+
+    if (
+      title === undefined &&
+      genre === undefined &&
+      director === undefined &&
+      releaseYear === undefined &&
+      language === undefined
+    ) {
+      return reply.code(400).send({
+        error:
+          "At least one field (title, genre, director, releaseYear or language) must be provided for update",
+      });
+    }
+
+    try {
+      const movie = await updateMovieById(fastify.db, movie_id, {
+        title,
+        genre,
+        director,
+        releaseYear,
+        language,
+      });
+      if (!movie) {
+        return reply.code(404).send({ error: "Movie not found" });
+      }
+
+      reply.send({ message: "Movie updated successfully.", movie });
+    } catch (error) {
+      reply.code(500).send({ error: error.message });
+    }
+  });
+
+  fastify.delete("/movies/:movie_id", async (request, reply) => {
+    const { movie_id } = request.params;
+
+    try {
+      const success = await deleteMovieById(fastify.db, movie_id);
+
+      if (!success) {
+        return reply.code(404).send({ error: "Movie not found" });
+      }
+
+      reply.send({ message: "Movie deleted successfully." });
+    } catch (error) {
+      reply.code(500).send({ error: error.message });
+    }
   });
 }
---
+++ src/services/movieService.js
@@ -0,0 +1,197 @@
+/**
+ * Creates a new movie
+ * @param {import("pg").Pool} db - The PostgreSQL Pool
+ * @param {Object} movieData - The movie data
+ * @param {string} [movieData.title] - Title of the movie
+ * @param {string} [movieData.genre] - Genre of the movie
+ * @param {string} [movieData.director] - Director of the movie
+ * @param {string|number} [movieData.releaseYear] - Release year of the movie
+ * @param {string} [movieData.language] - Original language of the movie
+ * @returns {Promise<Object>} The created movie
+ */
+export async function createMovie(
+  db,
+  { title, genre, director, releaseYear, language },
+) {
+  const query = `
+    INSERT INTO "Movie" ("Title", "Genre", "Director", "Release_Year", "Language")
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING "Movie_ID", "Title", "Genre", "Director", "Release_Year", "Language"
+    `;
+
+  const values = [title, genre, director, releaseYear, language];
+  const result = await db.query(query, values);
+  return result.rows[0];
+}
+
+/**
+ * Find movies in a paginated format with optional filters
+ * @param {import("pg").Pool} db - The PostgreSQL pool
+ * @param {number} page - Page number starting from 1
+ * @param {number} pageSize - Number of movies per page
+ * @param {Object} options - Additional options for filtering and sorting
+ * @param {string} [options.sortBy] - Field to "sort by"
+ * @param {string} [options.sortDirection="ASC"] - "ASC" or "DESC"
+ * @param {Object} [options.filters] - key-value pairs for filtering
+ * @returns {Promise<Object>} - Movies as an array and pagination details
+ */
+export async function getMovies(db, page = 1, pageSize = 10, options = {}) {
+  const { sortBy, sortDirection = "ASC", filters = {} } = options;
+
+  const offset = (page - 1) * pageSize;
+  const allowedColumns = new Set([
+    "Movie_ID",
+    "Title",
+    "Genre",
+    "Director",
+    "Release_Year",
+    "Language",
+  ]);
+  const filterList = [];
+  const filterValues = [];
+  let idx = 1;
+
+  for (const key in filters) {
+    if (Object.prototype.hasOwnProperty.call(filters, key)) {
+      const value = filters[key];
+
+      if (typeof value === "string") {
+        filterList.push(`"${key}" ILIKE $${idx++}`);
+        filterValues.push(`%${value}%`); // % regex ke liye
+      } else {
+        filterList.push(`"${key}" = $${idx++}`);
+        filterValues.push(value);
+      }
+    }
+  }
+
+  const whereClause =
+    filterList.length > 0 ? `WHERE ${filterList.join(" AND ")}` : "";
+
+  let orderClause = `ORDER BY "Movie_ID"`; // by default
+  if (sortBy && allowedColumns.has(sortBy)) {
+    const direction = sortDirection.toUpperCase() === "DESC" ? "DESC" : "ASC";
+    orderClause = `ORDER BY "${sortBy}" ${direction} `;
+  }
+
+  const filtersOnly = [...filterValues];
+
+  filterValues.push(pageSize, offset);
+  const limitPlaceholder = `${filterValues.length - 1}`;
+  const offsetPlaceholder = `${filterValues.length}`;
+
+  const query = `
+    SELECT "Movie_ID", "Title", "Genre", "Director", "Release_Year", "Language"
+    FROM "Movie"
+    ${whereClause}
+    ${orderClause} 
+    LIMIT $${limitPlaceholder} OFFSET $${offsetPlaceholder}
+    `;
+
+  const result = await db.query(query, filterValues);
+
+  const queryToFetchCount = `
+    SELECT COUNT(*) AS TOTAL FROM "Movie" ${whereClause}
+    `;
+
+  const totalCountResult = await db.query(queryToFetchCount, filtersOnly);
+  const totalCount = parseInt(totalCountResult.rows[0].total, 10);
+  const totalPages = Math.ceil(totalCount / pageSize);
+
+  return {
+    movies: result.rows,
+    page,
+    pageSize,
+    totalCount,
+    totalPages,
+  };
+}
+
+/**
+ * Find a movie by ID
+ * @param {import("pg").Pool} db - The PostgreSQL Pool
+ * @param {number|string} movieId - The movie's ID
+ * @returns {Promise<Object|null>} The movie data or null if not found
+ */
+export async function getMovieById(db, movieId) {
+  const query = `
+    SELECT "Movie_ID", "Title", "Genre", "Director", "Release_Year", "Language"
+    FROM "Movie"
+    WHERE "Movie_ID" = $1
+  `;
+  const result = await db.query(query, [movieId]);
+  return result.rows[0] || null;
+}
+
+/**
+ * Update a movie by ID
+ * @param {import("pg").Pool} db - The PostgreSQL Pool
+ * @param {number|string} movieId - The movie's ID
+ * @param {Object} movieData - The fields to update
+ * @param {string} [movieData.title] - Title of the movie
+ * @param {string} [movieData.genre] - Genre of the movie
+ * @param {string} [movieData.director] - Director of the movie
+ * @param {string|number} [movieData.releaseYear] - Release year of the movie
+ * @param {string} [movieData.language] - Original language of the movie
+ * @returns {Promise<Object|null>} The updated movie data or null if not found
+ */
+export async function updateMovieById(
+  db,
+  movieId,
+  { title, genre, director, releaseYear, language },
+) {
+  const updates = [];
+  const values = [];
+  let idx = 1;
+
+  if (title !== undefined) {
+    updates.push(`"Title" = $${idx++}`);
+    values.push(title);
+  }
+  if (genre !== undefined) {
+    updates.push(`"Genre" = $${idx++}`);
+    values.push(genre);
+  }
+  if (director !== undefined) {
+    updates.push(`"Director" = $${idx++}`);
+    values.push(director);
+  }
+  if (releaseYear !== undefined) {
+    updates.push(`"Release_Years" = $${idx++}`);
+    values.push(releaseYear);
+  }
+  if (language !== undefined) {
+    updates.push(`"Language" = $${idx++}`);
+    values.push(language);
+  }
+  if (updates.length === 0) {
+    throw new Error("No valid movie fields provided for updation");
+  }
+  values.push(movieId);
+
+  const query = `
+    UPDATE "Movie"
+    SET ${updates.join(", ")}
+    WHERE "Movie_ID" = $${idx}
+    RETURNING "Movie_ID", "Title", "Genre", "Director", "Release_Year", "Language"
+  `;
+  const result = await db.query(query, values);
+  return result.rows[0] || null;
+}
+
+/**
+ * Delete a movie by ID
+ * @param {import("pg").Pool} db - The PostgreSQL Pool
+ * @param {number|string} movieId - The movie's ID
+ * @returns {Promise<boolean>} true if movie was deleted
+ */
+export async function deleteMovieById(db, movieId) {
+  const query = `
+    DELETE FROM "Movie"
+    WHERE "Movie_ID" = $1
+    RETURNING "Movie_ID"
+  `;
+
+  const result = await db.query(query, [movieId]);
+  return result.rows.length > 0;
+}
