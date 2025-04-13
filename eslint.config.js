4f4c74fa0662b84f4186a49efaa44977f9729c30
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-04-14 05:29:13 +0530

    Ignore Linting fro browser static JS

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import { globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  globalIgnores(["public/**/*"]),
   pluginJs.configs.recommended,
   {
     languageOptions: { globals: globals.node },
